b850b55318f643b3da3411c4d14266e238a753b0
Shawn Wilson <acounting@taurenltd.com>
2018-04-30 01:06:51 -0600

    Adds Welcome Controller and Sets App Root

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :accounts, only: [:new, :create, :show]
 
+  root 'welcome#home'
+  match '/pricing', to: 'welcome#pricing', via: :get
+
 end
